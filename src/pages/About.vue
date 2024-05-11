f042f324330b240e574fd9a1cb14a991ed5eed8c
ahmed.shan <ahmedshan16@gmail.com>
2024-05-12 00:20:46 +0500

    Removed unwanted file

--- src/pages/About.vue
+++
@@ -1,9 +0,0 @@
-<template>
-    <SessionLayout>
-        <div>Hello about</div>
-    </SessionLayout>
-</template>
-
-<script setup>
-import SessionLayout from '@/layouts/SessionLayout.vue'
-</script>
