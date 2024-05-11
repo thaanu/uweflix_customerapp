ba8c3bc0d68eb2a8367a526457bdd0c2f78ac5c1
ahmed.shan <ahmedshan16@gmail.com>
2024-05-12 00:36:07 +0500

    Updated to bootstrap navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,22 +1,46 @@
 <template>
-    <nav>
-        <ul>
-            <li v-for="link in links">
-                <RouterLink :to="link.href">{{ link.name }}</RouterLink>
-            </li>
-        </ul>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">
+                <img src="@/assets/logos/main-color.jpg" alt="Bootstrap" class="rounded" width="30" height="30">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false"
+                aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li v-for="link in links" class="nav-item">
+                        <RouterLink :to="link.href" class="nav-link"><i :class="link.icon" style="margin-right: 5px;"></i>{{ link.name }}</RouterLink>
+                    </li>
+                </ul>
+            </div>
+        </div>
     </nav>
 </template>
 
 <script setup>
 const links = [
     {
-        name: 'Home',
+        name: 'Movies',
+        icon: 'fas fa-video',
         href: '/'
     },
     {
-        name: 'About',
-        href: '/about'
+        name: 'My Tickets',
+        icon: 'fas fa-ticket',
+        href: '/my-tickets'
+    },
+    {
+        name: 'Club House',
+        icon: 'fas fa-house-user',
+        href: '/club-house'
+    },
+    {
+        name: 'Signout',
+        icon: 'fas fa-right-to-bracket',
+        href: '/signout'
     },
 ]
 </script>
