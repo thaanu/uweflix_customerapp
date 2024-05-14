406019e33487660a5854687a7ea415d41094e4ba
ahmed.shan <ahmedshan16@gmail.com>
2024-05-14 18:53:05 +0500

    Added bootstrap and axios

--- src/main.js
+++ src/main.js
@@ -2,9 +2,12 @@ import '@/assets/css/style.css'
 import '@/assets/fs/css/all.min.css'
 
 import "bootstrap/dist/css/bootstrap.css";
+import "bootstrap/dist/js/bootstrap.js";
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue'
 import router from './router'
