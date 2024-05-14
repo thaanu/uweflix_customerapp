41510cab886a0d6a921d69e9b3baf3ae5f85ca34
ahmed.shan <ahmedshan16@gmail.com>
2024-05-15 04:03:03 +0500

    Refactored.. implemented sweetalerts

--- src/models/Auth.js
+++ src/models/Auth.js
@@ -1,13 +1,16 @@
-import {sendPost} from '@/models/ApiCall';
+import Swal from 'sweetalert2'
+import {sendPost, sendGet} from '@/models/ApiCall';
 const baseUrl = 'https://api.uweflix.naffah.me';
 export async function authenticate(email, password) {
-    
+
     const response = await sendPost(`${baseUrl}/token`, {username: email, password: password});
 
-    console.log('api_call_response', response);
-
     if ( response.response.status != 200 ) {
-        alert("Whoops");
+        Swal.fire({
+            title: "Umm",
+            text: "Unable to login",
+            icon: "error"
+        });
     } else {
         localStorage.setItem('user_token', response.data.access_token);
     }
