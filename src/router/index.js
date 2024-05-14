3855367aa11678322071e3a41e7b6fb57bf61482
ahmed.shan <ahmedshan16@gmail.com>
2024-05-14 18:52:05 +0500

    Added additional views

--- src/router/index.js
+++ src/router/index.js
@@ -1,7 +1,9 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import Login from '@/pages/Login.vue'
-import Home from '@/pages/Home.vue'
 import Movies from '@/pages/Movies.vue'
+import MyTickets from '@/pages/MyTickets.vue'
+import ClubHouse from '@/pages/ClubHouse.vue'
+import Signout from '@/pages/Signout.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,26 +14,34 @@ const router = createRouter({
       component: Login
     },
     {
-      path: '/home',
-      name: 'home',
-      component: Home
-    },
-    {
       path: '/movies',
       name: 'movies',
       component: Movies
+    },
+    {
+      path: '/my-tickets',
+      name: 'my-tickets',
+      component: MyTickets
+    },
+    {
+      path: '/club-house',
+      name: 'club-house',
+      component: ClubHouse
+    },
+    {
+      path: '/signout',
+      name: 'sign-out',
+      component: Signout
     }
   ]
 })
 
 router.beforeEach(async (to, from) => {
 
-  // const isAuthenticated = ( localStorage.getItem('user') == null ? false : true );
-
-  const isAuthenticated = true; // todo: Change this to dynamic login
+  const isAuthenticated = ( localStorage.getItem('user_token') == null ? false : true );
 
   // User authenticate and redirect to login page if unauthenticated
-  if ( !isAuthenticated && to.name !== 'Login' ) {
+  if ( !isAuthenticated && to.name !== 'login' ) {
     return { name: 'login' }
   }
 })
