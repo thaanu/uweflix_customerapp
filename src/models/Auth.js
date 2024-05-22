1c117c0063100303632ab14515a09bfca7b6191e
ahmed.shan <ahmedshan16@gmail.com>
2024-05-22 21:09:39 +0500

    Refactored to correct request status

--- src/models/Auth.js
+++ src/models/Auth.js
@@ -3,10 +3,10 @@ import {sendPost, sendGet} from '@/models/ApiCall';
 const baseUrl = 'https://api.uweflix.naffah.me';
 
 export async function authenticate(email, password) {
-
+    
     const response = await sendPost(`${baseUrl}/token`, {username: email, password: password});
 
-    if ( response.response.status != 200 ) {
+    if ( response.status != 201 ) {
         Swal.fire({
             title: "Umm",
             text: "Unable to login",
