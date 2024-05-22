815b4d592801c2e3d0424255828cdb5808c954d5
ahmed.shan <ahmedshan16@gmail.com>
2024-05-22 21:09:12 +0500

    Refactored to correct variables

--- src/components/MovieTag.vue
+++ src/components/MovieTag.vue
@@ -1,18 +1,18 @@
 <template>
     <div v-for="movie in movies" class="col-sm-6 col-lg-3 mb-3">
         <div class="card movie-card" @click="movie.show_desc=!movie.show_desc">
-            <img :src="movie.image" class="img-fluid card-img-top" :alt="movie.name">
+            <img v-if="!movie.poster_image" src="@/assets/dummy.jpeg" >
+            <img v-else :src="movie.poster_image" class="img-fluid card-img-top" :alt="movie.title">
             <div class="card-body">
-                <div style="position: absolute; background-color: white; top: 0; left: 0; padding: 10px; height: 100%;" v-show="movie.show_desc">{{ movie.desc }}</div>
+                <div style="position: absolute; background-color: white; top: 0; left: 0; padding: 10px; height: 100%; width: 100%;" v-show="movie.show_desc">{{ movie.trailer_desc }}</div>
                 <div class="mb-3">
                     <i class="fas fa-star text-warning"></i> {{ movie.rate }}
-                    <RouterLink style="margin-left: 10px;" :to="'/more-info/' + movie.uid"><i
+                    <RouterLink style="margin-left: 10px;" :to="'/more-info/' + movie.id"><i
                             style="margin-right: 5px;" class="fas fa-circle-info"></i></RouterLink>
                 </div>
-                <h6 class="mb-3">{{ movie.name }}</h6>
-
-                <RouterLink v-if="movie.available" class="d-block btn btn-outline-success"
-                    :to="'/purchase/' + movie.uid" style="margin-right: 5px;"><i class="fas fa-check-circle"
+                <h6 class="mb-3">{{ movie.title }}</h6>
+                <RouterLink v-if="movie.is_active" class="d-block btn btn-outline-success"
+                    :to="'/purchase/' + movie.id" style="margin-right: 5px;"><i class="fas fa-check-circle"
                         style="margin-right: 5px"></i>Buy Tickets</RouterLink>
                 <p v-else class="text-center fw-bold text-danger"><i class="fas fa-ban"></i> SOLD OUT</p>
             </div>
