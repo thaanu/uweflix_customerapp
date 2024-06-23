66098c035e3f0f71905db5ec8f77970a03e209c7
ahmed.shan <ahmedshan16@gmail.com>
2024-06-23 09:35:28 +0500

    initial commit

---
+++ src/components/Alerts.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="alert" :class="alerttype">
+        <i style="margin-right: 10px;" v-if="icon" :class="icon"></i>
+        {{ message }}
+    </div>
+</template>
+
+<script setup>
+const props = defineProps(['message', 'alerttype', 'icon']);
+</script>
