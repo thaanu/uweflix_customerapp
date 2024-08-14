08fbd967fcd47b3729222930328603e7f3614ec5
ahmed.shan <ahmedshan16@gmail.com>
2024-08-15 02:27:28 +0500

    added account type

--- src/pages/Purchase.vue
+++ src/pages/Purchase.vue
@@ -1,27 +1,3 @@
-<template>
-    <SessionLayout>
-        <div class="container my-5">
-            <h3 class="text-center mb-5 text-uppercase">Purchase Ticket</h3>
-            <div class="row">
-                <Alert v-if="serverError" :message="serverError" alerttype="alert-danger" icon="fas fa-circle-exclamation" />
-                <p v-if="isLoading"><i class="fas fa-spin fa-spinner"></i> Loading Movie Information</p>
-                <div class="mb-5">
-                    <MovieDetailedTag :movie="movie" />
-                </div>
-
-                <p v-if="isScheduleLoading"><i class="fas fa-spin fa-spinner"></i> Loading Available Schedule</p>
-                <div v-if="!isScheduleLoading">
-                    <p>Select a show</p>
-                    <p v-if="schedules.length == 0" class="alert alert-info"><i class="fas fa-ticket" style="margin-right: 10px"></i>No schedule available</p>
-                    <div style="display: flex; flex-flow: wrap; gap: 10px;">
-                        <RouterLink v-for="schedule in schedules" :to="'/purchase/seats/'+schedule.film_id+'/'+schedule.hall_id+'/'+schedule.id" class="btn btn-outline-primary btn-lg" style="width: 180px;">{{ schedule.formatted_showtime }}</RouterLink>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </SessionLayout>
-</template>
-
 <script setup>
 
 import { RouterLink, useRoute } from 'vue-router';
@@ -31,6 +7,11 @@ import MovieDetailedTag from '@/components/MovieDetailedTag.vue';
 import Alert from '@/components/Alerts.vue';
 import { onBeforeMount, ref } from 'vue';
 const baseURL = import.meta.env.VITE_API_BASE_URL;
+
+const user = JSON.parse(localStorage.getItem('user'));
+
+const isClubRap = ref(true);
+const accountType = ref('personal'); // by default account type is 'personal'
 
 const route = useRoute();
 
@@ -53,7 +34,6 @@ onBeforeMount( async () => {
         const newMovie = await getOneMovieById(selectedMovieId);
         newMovie.poster_image = `${baseURL}/films/images/image/${newMovie.id}-poster.jpg`;
         movie.value = newMovie;
-        console.log(movie.value)
     }
     catch ( err ) {
         serverError.value = 'Unable to fetch movie information';
@@ -83,4 +63,44 @@ onBeforeMount( async () => {
     isLoading.value = false
 } );
 
+const setAccountType = (accType) => {
+    accountType.value = accType;
+    localStorage.setItem('account_type', accountType.value);
+}
+
 </script>
+
+<template>
+    <SessionLayout>
+        <div class="container my-5">
+            <h3 class="text-center mb-5 text-uppercase">Purchase Ticket</h3>
+            <div class="row">
+                <Alert v-if="serverError" :message="serverError" alerttype="alert-danger" icon="fas fa-circle-exclamation" />
+                <p v-if="isLoading"><i class="fas fa-spin fa-spinner"></i> Loading Movie Information</p>
+                <div class="mb-5">
+                    <MovieDetailedTag :movie="movie" />
+                </div>
+
+                <!-- Show this option for club raps only -->
+                <template v-if="isClubRap">
+                    <div class="mb-5">
+                        <p>Select Account</p>
+                        <div class="d-flex">
+                            <button @click="setAccountType('personal')" style="margin-right: 10px;" :class="{'btn-primary':accountType=='personal'}" class="btn btn-lg">Personal Account</button>
+                            <button @click="setAccountType('club')" style="margin-right: 10px;" :class="{'btn-primary':accountType=='club'}" class="btn btn-lg">Club Account</button>
+                        </div>
+                    </div>
+                </template>
+
+                <p v-if="isScheduleLoading"><i class="fas fa-spin fa-spinner"></i> Loading Available Schedule</p>
+                <div v-if="!isScheduleLoading">
+                    <p>Select a show</p>
+                    <p v-if="schedules.length == 0" class="alert alert-info"><i class="fas fa-ticket" style="margin-right: 10px"></i>No schedule available</p>
+                    <div style="display: flex; flex-flow: wrap; gap: 10px;">
+                        <RouterLink v-for="schedule in schedules" :to="'/purchase/seats/'+schedule.film_id+'/'+schedule.hall_id+'/'+schedule.id" class="btn btn-outline-primary btn-lg" style="width: 180px;">{{ schedule.formatted_showtime }}</RouterLink>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </SessionLayout>
+</template>
